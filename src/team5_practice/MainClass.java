699ba4a814f69d585c3ed8f7f4f5c59ca23de64f
woojin97318 <woojin97318@naver.com>
2021-08-13 11:36:56 +0900

    MainClass 수정

--- src/team5_practice/MainClass.java
+++ src/team5_practice/MainClass.java
@@ -1,15 +1,13 @@
 package team5_practice;
 import java.util.Scanner;
 
-import com.care.airconditioner_jeonwujin.AirConditioner;
-import com.care.bath_parkseungjae.*;
 public class MainClass {
 
 	public static void main(String[] args) {
-		AirConditioner ac = new AirConditioner();
-		// 각자 객체 생성 위치 2
-		// 각자 객체 생성 위치 3
-		Bath b = new Bath();
+		// 에어컨 객체 생성 위치
+		// 전화기 객체 생성 위치
+		// 라디오 객체 생성 위치
+		// 욕조 객체 생성 위치
 
 		Scanner sc = new Scanner(System.in);
 		int inputNum = 0;
@@ -28,7 +26,7 @@ public class MainClass {
 			switch(inputNum) {
 			case 1 :
 				System.out.println("=== 에어컨 기능 ===");
-				ac.airConditionerFunc();
+				// 에어컨 메소드 위치
 				break;
 			case 2 :
 				System.out.println("=== 전화기 기능 ===");
@@ -40,7 +38,7 @@ public class MainClass {
 				break;
 			case 4 :
 				System.out.println("=== 욕조 기능 ===");
-				b.bath();
+				// 욕조 메소드 위치
 				break;
 			case 5 :
 				System.out.println("=== 프로그램 종료 ===");
